12db07648c1441fed32808006adcd32ed9dc784d
Ra4inA <vitya-sorokin-92@mail.ru>
2021-05-15 11:25:15 +0300

    Header

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.slider').slick({    
+    	autoplay: true,
+    	autoplaySpeed: 2000,    
+   	touchThreshold: 10,
+		speed: 300,
+		draggable: false,
+		swipe: false,
+		dots: true,
+  });
+});

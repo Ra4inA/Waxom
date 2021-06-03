63bc365fe8371e58162cb3c7d5fb62bb1541f3b0
Ra4inA <vitya-sorokin-92@mail.ru>
2021-06-03 11:43:09 +0300

    test

--- js/main.js
+++ js/main.js
@@ -9,3 +9,57 @@ $(function(){
 		dots: true,
   });
 });
+$(function(){
+  $('.post__img').slick({    
+    	autoplay: true,
+    	autoplaySpeed: 2000,
+    	slidesToShow: 1, 
+   	// touchThreshold: 10,
+		// speed: 300,
+		// draggable: false,
+		// swipe: false,
+		arrows: false,
+		dots: true,
+		// variableWidth: true,
+  });
+});
+
+//posts__items
+
+$(function(){
+  $('.posts__items').slick({    
+    	// autoplay: true,
+    	// autoplaySpeed: 2000,
+    	// centerMode: true,
+		arrows: true,
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		infinite: false,
+		 responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+
+        // slidesToScroll: 3,
+        // infinite: true,
+        // dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+ 
+        // slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+});

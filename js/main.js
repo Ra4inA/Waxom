b59126235f91d430a97ad8316f43b60f1504fc31
Ra4inA <vitya-sorokin-92@mail.ru>
2021-06-05 21:55:22 +0300

    animate

--- js/main.js
+++ js/main.js
@@ -48,10 +48,10 @@ $(function(){
 });
 
 
-	let $height;
 $(window).ready(function(){
 	if($(window).width() < 991){
 		$('.nav__list').append($('.cart'));
+		$("nav__list").removeClass("_animate _animate-bottom")
 	};
 
 	// if($(this).width < 500) {
@@ -62,10 +62,13 @@ $(window).ready(function(){
 });
 
 $(window).resize(function(){
+	console.log($(this).width());
 	if($(this).width() > 990){
-		$('.nav__icons').append($('.cart'));
+		$('.nav__icons').prepend($('.cart'));
+		$("nav__list").addClass("_animate _animate-bottom")
 	} else{
 		$('.nav__list').append($('.cart'));
+		$("nav__list").removeClass("_animate _animate-bottom")
 	}
 
 	// if($(this).width < 500) {
@@ -138,4 +141,22 @@ $(".footer__column-title").on("click", function(){
 			$(this).next().removeClass("_show");
 		};
 	};
+});
+
+$("section, footer").css("overflow-x", "hidden"); $(".presentation").css("overflow-x", "visible");
+$(".project > div").addClass(" _animate _animate-right _anim-no-hide")
+$(".footer__column-wrapper:first div").addClass("_animate _animate-left _anim-no-hide");
+$(".footer__column-wrapper_second div").addClass("_animate _animate-right _anim-no-hide");
+$("footer.columns div").addClass("_anim");
+const animItems = document.querySelectorAll("._animate");
+$("body").ready(function(){$(window).scrollTop(1);});
+$(window).on("scroll", function(){
+	for (let index = 0; index < animItems.length; index++){
+		if($(window).scrollTop() + $(window).height() > $(animItems[index]).offset().top + $(animItems[index]).height() / 2 
+			& $(window).scrollTop() < $(animItems[index]).offset().top + $(animItems[index]).height() / 2){
+			$(animItems[index]).addClass("_animate-Active");
+		} else {
+			if($(animItems[index]).hasClass("_anim-no-hide") !== true){$(animItems[index]).removeClass("_animate-Active");}
+		}
+	}
 });
